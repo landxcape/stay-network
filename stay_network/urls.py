63063225a8185eddda889a07b698bb403e40ccca
Ashish Shakya <38076182+landxcape@users.noreply.github.com>
2020-12-22 16:18:59 +0545

    update

--- stay_network/urls.py
+++ stay_network/urls.py
@@ -1,4 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 urlpatterns = [
@@ -7,4 +11,4 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("profile", views.profile, name="profile"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
