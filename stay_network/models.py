52569ed8d112060054dacbb6251703c7efa2bb30
Ashish Shakya <38076182+landxcape@users.noreply.github.com>
2020-12-31 18:09:01 +0545

    Rental model

--- stay_network/models.py
+++ stay_network/models.py
@@ -1,12 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.gis.db import models
 from django.db import models
-
-# Create your models here.
 
 
 class User(AbstractUser):
     pass
 
 
-class Post(models.Model):
-    pass
+class Rental(models.Model):
+    renter = models.ForeignKey(
+        "User", on_delete=models.CASCADE, related_name="rents"
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    location = models.CharField(max_length=255)
+    geo_location = models.PointField(geography=True, null=True)
+    no_of_rooms = models.PositiveIntegerField()
+    rent_type = models.CharField(max_length=255)
+    floor = models.PositiveIntegerField()
+    facilities = models.TextField(max_length=255)
+    parking = models.BooleanField(default=False)
+    availability = models.CharField(max_length=64)
+    timestamp = models.DateTimeField(auto_now_add=True)
