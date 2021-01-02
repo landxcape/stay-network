ca55111bc082fbd73417c463061a70b46ba2acf3
Ashish Shakya <38076182+landxcape@users.noreply.github.com>
2021-01-02 14:24:58 +0545

    Update urls.py

--- stay_network/urls.py
+++ stay_network/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("profile", views.profile, name="profile"),
+    path("<str:username>", views.profile, name="profile"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
